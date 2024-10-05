22c1024cb652f52aa6351de33829a64e19bb2a9d
DrinoSan <s.becirbegovic@gmail.com>
2024-10-05 08:14:03 +0200

    Building

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+set -xe
+
+clang `pkg-config --cflags raylib` -o main main.c `pkg-config --libs raylib` -lm -lpthread -L/opt/homebrew/lib -lglfw -framework CoreFoundation -framework Cocoa -framework IOKit -framework OpenGL
+
