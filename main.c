2ee47da502230dd0953222047214314ca94aef26
DrinoSan <s.becirbegovic@gmail.com>
2024-10-08 17:14:55 +0200

    Started real game

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include "raylib.h"
+#include <stdbool.h>
+#include <stdlib.h>
 
 //--------------------------------------------------------------------------------------
 #define NUM_SHOOTS      50
@@ -8,42 +10,17 @@
 #define THIRD_WAVE      50
 
 //--------------------------------------------------------------------------------------
-typedef enum
-{
-    FIRST = 0,
-    SECOND,
-    THIRD
-} EnemyWave;
-
-//--------------------------------------------------------------------------------------
 typedef struct Player
 {
     Rectangle rec;
     Vector2   speed;
     Color     color;
+    Vector2   startLinePos;
+    Vector2   endLinePos;
+    bool      isDrawing;
 } Player;
 
 //--------------------------------------------------------------------------------------
-typedef struct Enemy
-{
-    Rectangle rec;
-    Vector2   speed;
-    bool      active;
-    Color     color;
-} Enemy;
-
-//--------------------------------------------------------------------------------------
-typedef struct Shoot
-{
-    Rectangle rec;
-    Vector2   speed;
-    bool      active;
-    Color     color;
-} Shoot;
-
-//------------------------------------------------------------------------------------
-// Global Variables Declaration
-//------------------------------------------------------------------------------------
 static const int screenWidth  = 800;
 static const int screenHeight = 600;
 
@@ -51,22 +28,18 @@ static bool gameOver = false;
 static bool pause    = false;
 static int  score    = 0;
 static bool victory  = false;
-
-static Player    player                   = { 0 };
-static Enemy     enemy[ NUM_MAX_ENEMIES ] = { 0 };
-static Shoot     shoot[ NUM_SHOOTS ]      = { 0 };
-static EnemyWave wave                     = { 0 };
-
-static int   shootRate = 0;
+static bool  alphaDecrease = false;
 static float alpha     = 0.0f;
 
-static int  activeEnemies = 0;
-static int  enemiesKill   = 0;
-static bool smooth        = false;
+static Player player = { 0 };
+static Sound  firstSound;
+static Sound  secondSound;
+static Sound  endingSound;
+static bool   soundFlag;
 
-//------------------------------------------------------------------------------------
-// Module Functions Declaration (local)
-//------------------------------------------------------------------------------------
+////------------------------------------------------------------------------------------
+//// Module Functions Declaration (local)
+////------------------------------------------------------------------------------------
 static void InitGame( void );          // Initialize game
 static void UpdateGame( void );        // Update game (one frame)
 static void DrawGame( void );          // Draw game (one frame)
@@ -81,11 +54,12 @@ int main(void)
     const int screenHeight = 600;
 
     InitWindow( screenWidth, screenHeight, "Shoot me baby" );
+    InitAudioDevice();
+    SetMasterVolume( 100.0 );
 
     InitGame();
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+    SetTargetFPS( 60 );   // Set our game to run at 60
 
     // Main game loop
     while ( !WindowShouldClose() )   // Detect window close button or ESC key
@@ -97,26 +71,21 @@ int main(void)
 
     // De-Initialization
     //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
+    CloseWindow();   // Close window and OpenGL context
+    CloseAudioDevice();
     //--------------------------------------------------------------------------------------
 
     return 0;
 }
 
-// Initialize game variables
 void InitGame( void )
 {
     // Initialize game variables
-    shootRate     = 0;
-    pause         = false;
-    gameOver      = false;
-    victory       = false;
-    smooth        = false;
-    wave          = FIRST;
-    activeEnemies = FIRST_WAVE;
-    enemiesKill   = 0;
-    score         = 0;
-    alpha         = 0;
+    pause    = false;
+    gameOver = false;
+    victory  = false;
+    score    = 0;
+    alpha    = 0;
 
     // Initialize player
     player.rec.x      = 20;
@@ -127,34 +96,40 @@ void InitGame( void )
     player.speed.y    = 5;
     player.color      = BLACK;
 
-    // Initialize enemies
-    for ( int i = 0; i < NUM_MAX_ENEMIES; i++ )
+    player.startLinePos.x = 0;
+    player.startLinePos.y = 0;
+    player.endLinePos.x   = 0;
+    player.endLinePos.y   = 0;
+    player.isDrawing      = false;
+
+    Wave w1 = LoadWave( "sound/First.wav" );
+    Wave w2 = LoadWave( "sound/Second.wav" );
+    Wave w3 = LoadWave( "sound/Ending.wav" );
+    if ( !IsWaveReady( w1 ) || !IsWaveReady( w2 ) )
     {
-        enemy[ i ].rec.width  = 10;
-        enemy[ i ].rec.height = 10;
-        enemy[ i ].rec.x = GetRandomValue( screenWidth, screenWidth + 1000 );
-        enemy[ i ].rec.y =
-            GetRandomValue( 0, screenHeight - enemy[ i ].rec.height );
-        enemy[ i ].speed.x = 5;
-        enemy[ i ].speed.y = 5;
-        enemy[ i ].active  = true;
-        enemy[ i ].color   = GRAY;
+        // Well stuff happens
+        exit( 1 );
     }
 
-    // Initialize shoots
-    for ( int i = 0; i < NUM_SHOOTS; i++ )
-    {
-        shoot[ i ].rec.x      = player.rec.x;
-        shoot[ i ].rec.y      = player.rec.y + player.rec.height / 4;
-        shoot[ i ].rec.width  = 10;
-        shoot[ i ].rec.height = 5;
-        shoot[ i ].speed.x    = 7;
-        shoot[ i ].speed.y    = 0;
-        shoot[ i ].active     = false;
-        shoot[ i ].color      = MAROON;
-    }
+    firstSound  = LoadSoundFromWave( w1 );
+    secondSound = LoadSoundFromWave( w2 );
+    endingSound = LoadSoundFromWave( w3 );
+
+    SetSoundVolume( firstSound, 100.0 );
+    SetSoundVolume( secondSound, 100.0 );
+    SetSoundVolume( endingSound, 100.0 );
+    soundFlag = true;
 }
 
+//--------------------------------------------------------------------------------------
+// Update and Draw (one frame)
+void UpdateDrawFrame( void )
+{
+    UpdateGame();
+    DrawGame();
+}
+
+//--------------------------------------------------------------------------------------
 // Update game (one frame)
 void UpdateGame( void )
 {
@@ -163,188 +138,89 @@ void UpdateGame( void )
         if ( IsKeyPressed( 'P' ) )
             pause = !pause;
 
-        if ( !pause )
+        if ( pause )
         {
-            switch ( wave )
-            {
-            case FIRST:
-            {
-                if ( !smooth )
-                {
-                    alpha += 0.02f;
+            return;
+        }
 
-                    if ( alpha >= 1.0f )
-                        smooth = true;
-                }
+        if ( alphaDecrease == false )
+        {
+            alpha += 0.02f;
+        }
+        else
+        {
+            alpha -= 0.02f;
+        }
 
-                if ( smooth )
-                    alpha -= 0.02f;
+        if ( alpha >= 1.00f )
+        {
+            alphaDecrease = true;
+        }
+        else if ( alpha <= 0.00f )
+        {
+            alphaDecrease = false;
+        }
 
-                if ( enemiesKill == activeEnemies )
-                {
-                    enemiesKill = 0;
+        if ( IsKeyPressed( KEY_SPACE ) && player.isDrawing == true )
+        {
+            player.isDrawing = false;
+        }
 
-                    for ( int i = 0; i < activeEnemies; i++ )
-                    {
-                        if ( !enemy[ i ].active )
-                            enemy[ i ].active = true;
-                    }
+        if ( IsKeyPressed( KEY_SPACE ) && player.isDrawing == false )
+        {
+            player.startLinePos.x = player.rec.x;
+            player.startLinePos.y = player.rec.y;
+            player.isDrawing      = true;
+        }
 
-                    activeEnemies = SECOND_WAVE;
-                    wave          = SECOND;
-                    smooth        = false;
-                    alpha         = 0.0f;
-                }
-            }
-            break;
-            case SECOND:
-            {
-                if ( !smooth )
-                {
-                    alpha += 0.02f;
+        if ( player.isDrawing == true )
+        {
+            player.endLinePos.x = player.rec.x;
+            player.endLinePos.y = player.rec.y;
+        }
 
-                    if ( alpha >= 1.0f )
-                        smooth = true;
-                }
+        // Player movement
+        if ( IsKeyDown( KEY_RIGHT ) )
+        {
+            player.rec.x += player.speed.x;
+            score += player.rec.x;
+        }
+        if ( IsKeyDown( KEY_LEFT ) )
+        {
+            player.rec.x -= player.speed.x;
+            score -= player.rec.x;
+        }
+        if ( IsKeyDown( KEY_UP ) )
+        {
+            player.rec.y -= player.speed.y;
+            score += player.rec.y;
+        }
+        if ( IsKeyDown( KEY_DOWN ) )
+        {
+            player.rec.y += player.speed.y;
+            score -= player.rec.y;
+        }
 
-                if ( smooth )
-                    alpha -= 0.02f;
-
-                if ( enemiesKill == activeEnemies )
-                {
-                    enemiesKill = 0;
-
-                    for ( int i = 0; i < activeEnemies; i++ )
-                    {
-                        if ( !enemy[ i ].active )
-                            enemy[ i ].active = true;
-                    }
-
-                    activeEnemies = THIRD_WAVE;
-                    wave          = THIRD;
-                    smooth        = false;
-                    alpha         = 0.0f;
-                }
-            }
-            break;
-            case THIRD:
-            {
-                if ( !smooth )
-                {
-                    alpha += 0.02f;
-
-                    if ( alpha >= 1.0f )
-                        smooth = true;
-                }
-
-                if ( smooth )
-                    alpha -= 0.02f;
-
-                if ( enemiesKill == activeEnemies )
-                    victory = true;
-            }
-            break;
-            default:
-                break;
-            }
-
-            // Player movement
-            if ( IsKeyDown( KEY_RIGHT ) )
-                player.rec.x += player.speed.x;
-            if ( IsKeyDown( KEY_LEFT ) )
-                player.rec.x -= player.speed.x;
-            if ( IsKeyDown( KEY_UP ) )
-                player.rec.y -= player.speed.y;
-            if ( IsKeyDown( KEY_DOWN ) )
-                player.rec.y += player.speed.y;
-
-            // Player collision with enemy
-            for ( int i = 0; i < activeEnemies; i++ )
-            {
-                if ( CheckCollisionRecs( player.rec, enemy[ i ].rec ) )
-                    gameOver = true;
-            }
-
-            // Enemy behaviour
-            for ( int i = 0; i < activeEnemies; i++ )
-            {
-                if ( enemy[ i ].active )
-                {
-                    enemy[ i ].rec.x -= enemy[ i ].speed.x;
-
-                    if ( enemy[ i ].rec.x < 0 )
-                    {
-                        enemy[ i ].rec.x =
-                            GetRandomValue( screenWidth, screenWidth + 1000 );
-                        enemy[ i ].rec.y = GetRandomValue(
-                            0, screenHeight - enemy[ i ].rec.height );
-                    }
-                }
-            }
-
-            // Wall behaviour
-            if ( player.rec.x <= 0 )
-                player.rec.x = 0;
-            if ( player.rec.x + player.rec.width >= screenWidth )
-                player.rec.x = screenWidth - player.rec.width;
-            if ( player.rec.y <= 0 )
-                player.rec.y = 0;
-            if ( player.rec.y + player.rec.height >= screenHeight )
-                player.rec.y = screenHeight - player.rec.height;
-
-            // Shoot initialization
-            if ( IsKeyDown( KEY_SPACE ) )
-            {
-                shootRate += 5;
-
-                for ( int i = 0; i < NUM_SHOOTS; i++ )
-                {
-                    if ( !shoot[ i ].active && shootRate % 20 == 0 )
-                    {
-                        shoot[ i ].rec.x = player.rec.x;
-                        shoot[ i ].rec.y = player.rec.y + player.rec.height / 4;
-                        shoot[ i ].active = true;
-                        break;
-                    }
-                }
-            }
-
-            // Shoot logic
-            for ( int i = 0; i < NUM_SHOOTS; i++ )
-            {
-                if ( shoot[ i ].active )
-                {
-                    // Movement
-                    shoot[ i ].rec.x += shoot[ i ].speed.x;
-
-                    // Collision with enemy
-                    for ( int j = 0; j < activeEnemies; j++ )
-                    {
-                        if ( enemy[ j ].active )
-                        {
-                            if ( CheckCollisionRecs( shoot[ i ].rec,
-                                                     enemy[ j ].rec ) )
-                            {
-                                shoot[ i ].active = false;
-                                enemy[ j ].rec.x  = GetRandomValue(
-                                    screenWidth, screenWidth + 1000 );
-                                enemy[ j ].rec.y = GetRandomValue(
-                                    0, screenHeight - enemy[ j ].rec.height );
-                                shootRate = 0;
-                                enemiesKill++;
-                                score += 100;
-                            }
-
-                            if ( shoot[ i ].rec.x + shoot[ i ].rec.width >=
-                                 screenWidth )
-                            {
-                                shoot[ i ].active = false;
-                                shootRate         = 0;
-                            }
-                        }
-                    }
-                }
-            }
+        // Wall behaviour
+        if ( player.rec.x <= 0 )
+        {
+            score        = 0;
+            player.rec.x = 0;
+        }
+        if ( player.rec.x + player.rec.width >= screenWidth )
+        {
+            score        = 0;
+            player.rec.x = screenWidth - player.rec.width;
+        }
+        if ( player.rec.y <= 0 )
+        {
+            score        = 0;
+            player.rec.y = 0;
+        }
+        if ( player.rec.y + player.rec.height >= screenHeight )
+        {
+            score        = 0;
+            player.rec.y = screenHeight - player.rec.height;
         }
     }
     else
@@ -357,6 +233,7 @@ void UpdateGame( void )
     }
 }
 
+//--------------------------------------------------------------------------------------
 // Draw game (one frame)
 void DrawGame( void )
 {
@@ -368,39 +245,21 @@ void DrawGame( void )
     {
         DrawRectangleRec( player.rec, player.color );
 
-        if ( wave == FIRST )
-            DrawText( "FIRST WAVE",
-                      screenWidth / 2 - MeasureText( "FIRST WAVE", 40 ) / 2,
-                      screenHeight / 2 - 40, 40, Fade( BLACK, alpha ) );
-        else if ( wave == SECOND )
-            DrawText( "SECOND WAVE",
-                      screenWidth / 2 - MeasureText( "SECOND WAVE", 40 ) / 2,
-                      screenHeight / 2 - 40, 40, Fade( BLACK, alpha ) );
-        else if ( wave == THIRD )
-            DrawText( "THIRD WAVE",
-                      screenWidth / 2 - MeasureText( "THIRD WAVE", 40 ) / 2,
-                      screenHeight / 2 - 40, 40, Fade( BLACK, alpha ) );
+        DrawLineV( player.startLinePos, player.endLinePos, RED );
 
-        for ( int i = 0; i < activeEnemies; i++ )
-        {
-            if ( enemy[ i ].active )
-                DrawRectangleRec( enemy[ i ].rec, enemy[ i ].color );
-        }
-
-        for ( int i = 0; i < NUM_SHOOTS; i++ )
-        {
-            if ( shoot[ i ].active )
-                DrawRectangleRec( shoot[ i ].rec, shoot[ i ].color );
-        }
+        DrawText( "Private Parts",
+                  screenWidth / 2 - MeasureText( "Private Parts", 40 ) / 2,
+                  screenHeight / 2 - 40, 40, Fade( BLACK, alpha ) );
 
         DrawText( TextFormat( "%04i", score ), 20, 20, 40, GRAY );
         // Draw Variables
         TraceLog( LOG_INFO, "----------------------------------------" );
-        TraceLog( LOG_INFO, "ShootRate:     %d", shootRate );
         TraceLog( LOG_INFO, "Alpha:         %f", alpha );
-        TraceLog( LOG_INFO, "activeEnemies: %d", activeEnemies );
-        TraceLog( LOG_INFO, "enemiesKill:   %d", enemiesKill );
-        TraceLog( LOG_INFO, "smooth:        %d", smooth );
+        TraceLog( LOG_INFO, "DeawingStart x:  %d", player.startLinePos.x );
+        TraceLog( LOG_INFO, "DeawingStart y:  %d", player.startLinePos.y );
+        TraceLog( LOG_INFO, "DeawingEND x:    %d", player.endLinePos.x );
+        TraceLog( LOG_INFO, "DeawingEND y:    %d", player.endLinePos.y );
+        TraceLog( LOG_INFO, "GetMasterVolume:        %d", GetMasterVolume() );
         TraceLog( LOG_INFO, "++++++++++++++++++++++++++++++++++++++++" );
 
         if ( victory )
@@ -414,23 +273,19 @@ void DrawGame( void )
                       screenHeight / 2 - 40, 40, GRAY );
     }
     else
+    {
         DrawText( "PRESS [ENTER] TO PLAY AGAIN",
                   GetScreenWidth() / 2 -
                       MeasureText( "PRESS [ENTER] TO PLAY AGAIN", 20 ) / 2,
                   GetScreenHeight() / 2 - 50, 20, GRAY );
+    }
 
     EndDrawing();
 }
 
-// Unload game variables
+//--------------------------------------------------------------------------------------
 void UnloadGame( void )
 {
     // TODO: Unload all dynamic loaded data (textures, sounds, models...)
 }
 
-// Update and Draw (one frame)
-void UpdateDrawFrame( void )
-{
-    UpdateGame();
-    DrawGame();
-}
